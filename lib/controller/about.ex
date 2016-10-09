fe6d45f2c9b0c7efdb175d8c55c8d2e5d4d34ce2
Jeff Kantarek <jeff.kantarek@gmail.com>
2016-10-09 09:40:27 -0500

    [CREDO] alias external dependancies

--- lib/controller/about.ex
+++ lib/controller/about.ex
@@ -1,13 +1,14 @@
 defmodule Controller.About do
+  alias Response.Json, as: Json
   use Plug.Router
   plug :match
   plug :dispatch
 
   get "/:name" do
-    Response.Json.render(conn, body: %{name: name, stuff: "foo"})
+    Json.render(conn, body: %{name: name, stuff: "foo"})
   end
 
   match _ do
-    Response.Json.render(conn, status: 404)
+    Json.render(conn, status: 404)
   end
 end
