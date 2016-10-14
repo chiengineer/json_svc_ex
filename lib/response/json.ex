b3be5c27d9dd42d82bd784c95a6c56ca4d4b6e92
Jeff Kantarek <jeff.kantarek@gmail.com>
2016-10-13 22:07:56 -0500

    [JSON] add json parse helper

--- lib/response/json.ex
+++ lib/response/json.ex
@@ -11,12 +11,21 @@ defmodule Response.Json do
   def render(conn, status: status), do: rend(conn, %{}, status)
   def render(conn, body: body, status: status), do: rend(conn, body, status)
 
+  def parse(conn, type: type), do: prse(conn, options: %{as: type})
+  def parse(conn),             do: prse(conn, options: %{})
+
   #Private
 
   defp rend(conn, body, status) do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(status, Poison.encode!(body))
+  end
+
+  alias Plug.Conn, as: Connection
+  defp prse(conn, options: options) do
+    {:ok, body, _} = Connection.read_body(conn, length: 1_000_000)
+    Poison.decode!(body, options)
   end
 
 end
