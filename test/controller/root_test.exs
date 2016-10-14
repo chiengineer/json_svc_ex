819c93a7f618aab15a933f517e380d593736681c
Jeff Kantarek <jeff.kantarek@gmail.com>
2016-10-13 22:05:28 -0500

    [SPEC] re-organize controller tests

--- test/controller/root_test.exs
+++ test/controller/root_test.exs
@@ -1,4 +1,4 @@
-defmodule JsonSvcTest do
+defmodule Controller.RootTest do
   use ExUnit.Case, async: true
   use Plug.Test
 
@@ -21,23 +21,6 @@ defmodule JsonSvcTest do
     assert String.match?(conn.resp_body, ~r/ok/)
   end
 
-  test "GET /about/:foo returns foo" do
-    conn = conn(:get, "/about/foo")
-    conn = Router.Base.call(conn, @opts)
-
-    assert conn.state == :sent
-    assert conn.status == 200
-    assert String.match?(conn.resp_body, ~r/foo/)
-  end
-
-  test "GET /about/:foo/bar returns 404" do
-    conn = conn(:get, "/about/foo/bar")
-    conn = Router.Base.call(conn, @opts)
-
-    assert conn.state == :sent
-    assert conn.status == 404
-  end
-
   test "GET /randomunknownroute returns 404" do
     conn = conn(:get, "/randomunknownroute")
     conn = Router.Base.call(conn, @opts)
@@ -45,4 +28,5 @@ defmodule JsonSvcTest do
     assert conn.state == :sent
     assert conn.status == 404
   end
+
 end
