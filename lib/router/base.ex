874af8bfc90209b51ccdc32a5e94cee9fe3dd36d
Jeff Kantarek <jeff.kantarek@gmail.com>
2016-10-13 22:09:35 -0500

    [JSON] handle raised errors gracefully

--- lib/router/base.ex
+++ lib/router/base.ex
@@ -4,10 +4,22 @@ defmodule Router.Base do
   each controller's defined path
   """
   use Plug.Router
+  use Plug.ErrorHandler
+
+  alias Response.Json, as: Json
 
   plug :match
   plug :dispatch
 
   forward "/about", to: Controller.About
   forward "/",      to: Controller.Root
+
+  def handle_errors(conn, %{kind: kind, reason: reason, stack: _stack}) do
+    status = set_status_from_kind(kind)
+    Json.fail(conn, status, message: reason)
+  end
+
+  defp set_status_from_kind(kind) do
+    if kind == :throw, do: 400, else: 500
+  end
 end
