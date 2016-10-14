1d54c22eb055eef2782c760df388471375c7be4b
Paul Sernatinger <pauls@trunkclub.com>
2016-10-14 16:23:57 -0500

    [VALIDATE] use dynamic validation

--- lib/model/account/request.ex
+++ lib/model/account/request.ex
@@ -3,11 +3,29 @@ defmodule Model.Account.Request do
   Account request object
   """
   @derive [Poison.Encoder]
-  defstruct [:first_name, :last_name, :email]
+  @required_keys [:first_name, :last_name, :email]
+  defstruct @required_keys
 
   def validate(request) do
-    unless request.first_name, do: throw [message: "First Name Missing", http_code: 400]
-    unless request.last_name, do: throw [message: "Last Name Missing", http_code: 400]
-    unless request.email, do: throw [message: "Email Missing", http_code: 400]
+    validate_array = validate_keysp(@required_keys, request)
+    error_keys = Enum.filter(
+      validate_array,
+      fn tuple -> elem(tuple, 1) == false end)
+    if length(error_keys) > 0,
+      do: throw [message: generate_error_messages(error_keys), http_code: 400]
+  end
+
+  defp generate_error_messages(errors) do
+    Enum.map(errors, fn err -> "#{elem(err, 0)} is missing." end)
+  end
+
+  defp validate_keysp(required, request) do
+    Enum.map(
+      required,
+      fn(k) -> key_presentp(k, Map.get(request, k)) end)
+  end
+
+  defp key_presentp(key, value) do
+    {key, value != nil}
   end
 end
