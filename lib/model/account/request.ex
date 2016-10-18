8fa034ceca0b3222bfb3bfbd2441ac785d00006d
Jeff Kantarek <jeff.kantarek@gmail.com>
2016-10-17 21:53:31 -0500

    [SPEC] doctests and validation testing

--- lib/model/account/request.ex
+++ lib/model/account/request.ex
@@ -1,18 +1,32 @@
 defmodule Model.Account.Request do
   @moduledoc """
   Account request object
+
+  ## Examples
+
+  iex> test_model = %Model.Account.Request{
+  ...> first_name: "Joe",
+  ...> last_name: "Bob",
+  ...> email: "joe@bob.com"}
+  iex> Model.Account.Request.validate!(test_model)
+  {:ok, :valid}
   """
   @derive [Poison.Encoder]
   @required_keys [:first_name, :last_name, :email]
   defstruct @required_keys
 
-  def validate(request) do
+  def validate!(request) do
     validate_array = validate_keysp(@required_keys, request)
+    throw_error_for_invalid_entries!(validate_array)
+    {:ok, :valid}
+  end
+
+  defp throw_error_for_invalid_entries!(validate_array) do
     error_keys = Enum.filter(
       validate_array,
       fn tuple -> elem(tuple, 1) == false end)
     if length(error_keys) > 0,
-      do: throw [message: generate_error_messages(error_keys), http_code: 400]
+      do: raise [message: generate_error_messages(error_keys), http_code: 400]
   end
 
   defp generate_error_messages(errors) do
