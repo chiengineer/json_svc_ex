4961980a6dec62af77158974fa3b4e031f544a0d
Jeff Kantarek <jeffk@trunkclub.com>
2016-11-17 14:53:29 -0600

    [KAFKA][SETUP] add kafka worker support

--- config/dev.exs
+++ config/dev.exs
@@ -7,3 +7,14 @@ config :honeybadger,
 config :logger,
   backends: [:console],
   compile_time_purge_level: :info
+
+#update this with your relevant host details or use dev.overrides.exs
+
+config :kafka_ex,
+  brokers: [{"localhost", 9092}],
+  consumer_group: "kafka_ex",
+  disable_default_worker: false,
+  sync_timeout: 3000,
+  max_restarts: 10,
+  max_seconds: 60,
+  kafka_version: "0.9.0"
