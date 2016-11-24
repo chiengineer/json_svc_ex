e284961daa67dca3036216cb091eea9f1fd70159
Jeff Kantarek <jeffk@trunkclub.com>
2016-11-24 11:42:35 -0600

    [DOC] add docspecs for `KafkaHandelrs.Workers`

--- lib/kafka_handlers/workers.ex
+++ lib/kafka_handlers/workers.ex
@@ -4,11 +4,15 @@ defmodule KafkaHandlers.Workers do
   @moduledoc """
   Kafka Workers - create workers with atoms for handling different controller
   streams
-  @workers list of worker modules that contain interface kafka_meta
-  kafka_meta - %{worker_id: identifier for worker to use}
+  `@workers` list of worker modules that contain interface kafka_meta
   """
   @workers [KafkaHandlers.Account.RequestCreate]
 
+  @doc """
+    This function creates workers injected from the array `@workers` workers
+    _must_ impliment `.kafka_meta[:worker_id]`
+  """
+  @spec create_workers() :: [pid]
   def create_workers(handler \\ &KafkaEx.create_worker/1) do
     Enum.map(
       fetch_worker_ids,
@@ -16,6 +20,7 @@ defmodule KafkaHandlers.Workers do
     )
   end
 
+  @spec fetch_worker_ids() :: [atom]
   defp fetch_worker_ids do
      ids = Enum.map(
       @workers,
@@ -24,6 +29,7 @@ defmodule KafkaHandlers.Workers do
      Enum.uniq(ids)
   end
 
+  @spec fetch_worker_id(module) :: atom
   defp fetch_worker_id(worker) do
      worker.kafka_meta[:worker_id]
   end
