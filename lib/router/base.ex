31e860ea5ee6e8ca4804880408fa9a79d609c719
Jeff Kantarek <jeffk@trunkclub.com>
2016-11-25 14:50:02 -0600

    [DOCS] add documentation to `Router.Base`

--- lib/router/base.ex
+++ lib/router/base.ex
@@ -2,6 +2,11 @@ defmodule Router.Base do
   @moduledoc """
   Base Router, handles all forwarding requests to separate controllers based on
   each controller's defined path
+
+  # Routes
+  - `/about` - forwards to `Controller.About`
+  - `/account` - forwards to `Controller.Account`
+  - `/` - forwards to `Controller.Root`
   """
   use Plug.Router
   use Plug.ErrorHandler
@@ -16,6 +21,13 @@ defmodule Router.Base do
   forward "/account", to: Controller.Account
   forward "/",      to: Controller.Root
 
+  @spec handle_errors(Plug.Conn, list) :: no_return
+  @doc """
+  raised errors from http requests are handled here
+
+  - `Response.Json.fail/2` sent to `Plug.Conn` client
+  - error reported via `ErrorReporter.Honeybadger`
+  """
   def handle_errors(conn, %{kind: kind, reason: reason, stack: stack}) do
     Json.fail(conn, reason)
     ErrorReporter.report(
