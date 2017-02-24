278c0844b1d1d3b64516194ff4ba2480235b43a0
Jeff Kantarek <jeff.kantarek@gmail.com>
2017-02-23 19:48:41 -0600

    [STATIC] clean up honeybadger reporter

--- lib/error_reporter/honeybadger.ex
+++ lib/error_reporter/honeybadger.ex
@@ -24,7 +24,12 @@ defmodule ErrorReporter.Honeybadger do
 
   @spec report(Exception.t) :: {:ok, :sent}
   @spec report(Exception.t, %{}) :: {:ok, :sent}
-  @spec report(Exception.t, context: %{}, stacktrace: %{}, options: %{ignore: boolean}) :: {:ok, :ignored}
+  @spec report(
+    Exception.t,
+    context: %{},
+    stacktrace: %{},
+    options: %{ignore: boolean}
+  ) :: {:ok, :ignored}
 
 
   def report(exception), do: reportp(exception, nil, nil)
