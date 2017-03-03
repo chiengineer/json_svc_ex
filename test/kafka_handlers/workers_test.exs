7e80f7285e4340ca8700b15fbbec46ab41f48a57
Jeff Kantarek <jeff.kantarek@gmail.com>
2017-03-03 08:22:38 -0600

    [KAFKA] KafkaHandlers.Workers use Kafka.Helper

--- test/kafka_handlers/workers_test.exs
+++ test/kafka_handlers/workers_test.exs
@@ -3,11 +3,13 @@ defmodule KafkaHandlers.WorkersTest do
   alias KafkaHandlers.Workers, as: Workers
 
   test "all kafka workers are created" do
-    worker_handler = fn worker_id ->
+    worker_handler = fn (worker_id, [consumer_group: _]) ->
       send self(), worker_id
     end
+
     Workers.create_workers(worker_handler)
     assert_received :account_controller_stream
+    assert_received :account_handle_request_create_stream
     refute_received _
   end
 
