f4d19f32060aa72f818036a6e434cb716c14a50c
Jeff Kantarek <jeff.kantarek@gmail.com>
2017-03-06 22:04:06 -0600

    [CLEANUP] use Kafka.Helpers in KafkaHandlers.Account.RequestCreate

--- lib/kafka_handlers/account/request_create.ex
+++ lib/kafka_handlers/account/request_create.ex
@@ -6,6 +6,7 @@ defmodule KafkaHandlers.Account.RequestCreate do
 
   TODO: include pattern matching for versioned topics
   """
+  alias Kafka.Helpers, as: Helper
 
   @worker_id :account_controller_stream
   @topic_model "Account"
@@ -35,10 +36,20 @@ defmodule KafkaHandlers.Account.RequestCreate do
     `KafkaEx`
   """
 
-  def publish(account_payload, timestamp: time), do: publishp(account_payload, format_time(time))
-  def publish(account_payload, handler: handler), do: publishp(account_payload, format_time(now()), handler)
-  def publish(account_payload, timestamp: time, request_id: id, handler: handler), do: publishp(account_payload, format_time(time), handler, id)
-  def publish(account_payload), do: publishp(account_payload, format_time(now()))
+  def publish(account_payload, timestamp: time) do
+    publishp(account_payload, Helper.format_time(time))
+  end
+
+  def publish(account_payload, handler: handler) do
+    publishp(account_payload, Helper.now_string(), handler)
+  end
+
+  def publish(payload, timestamp: time, request_id: id, handler: handler) do
+    publishp(payload, Helper.format_time(time), handler, id)
+  end
+  def publish(account_payload) do
+    publishp(account_payload, Helper.now_string())
+  end
 
   @spec kafka_meta() :: %{
     worker_id: atom(), partitions: Integer.t, topic: String.t
@@ -53,57 +64,15 @@ defmodule KafkaHandlers.Account.RequestCreate do
 
   @spec publishp(map, String.t, fun, String.t) :: {:ok, map}
   defp publishp(payload, timestamp, handler \\ &KafkaEx.produce/4, uuid \\ UUID.uuid4()) do
-    {:ok, valid_uuid} = validate_uuid(uuid)
-    normalized_payload = payload_normalizer(payload, timestamp, valid_uuid)
+    {:ok, valid_uuid} = Helper.validate_uuid(uuid)
+    normalized_payload =
+      Helper.payload_normalizer(payload, timestamp, valid_uuid)
     :ok = handler.(
       @topic,
-      select_random_partition(),
-      encode_payload_request(normalized_payload),
+      Helper.select_random_partition(@partitions),
+      Helper.encode_payload_request(normalized_payload),
       worker_name: @worker_id
     )
     {:ok, normalized_payload}
-  end
-
-  @spec validate_uuid(String.t) :: {:ok, String.t}
-  defp validate_uuid(uuid) do
-    [
-      {:uuid, valid_uuid},
-      {:binary, _},
-      {:type, :default},
-      {:version, 4},
-      {:variant, :rfc4122}
-    ] = UUID.info!(uuid)
-    {:ok, valid_uuid}
-  end
-
-  @spec select_random_partition() :: integer
-  defp select_random_partition do
-    Enum.random(@partitions)
-  end
-
-  @spec now() :: DateTime
-  defp now do
-   DateTime.utc_now
-  end
-
-  @spec format_time(DateTime.t) :: String.t
-  defp format_time(datetime) do
-     DateTime.to_iso8601(datetime)
-  end
-
-  @spec payload_normalizer(map, String.t, String.t) :: map
-  defp payload_normalizer(payload, timestamp, uuid) do
-    %{
-      meta: %{
-        requested_at: timestamp,
-        transaction_id: uuid
-      },
-      request_body: payload
-    }
-  end
-
-  @spec encode_payload_request(map) :: String.t
-  defp encode_payload_request(normalized_payload) do
-    Poison.encode!(normalized_payload)
   end
 end
