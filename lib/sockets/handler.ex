65397c4f11deee167bd33b5538ed14556dc27fbf
Jeff Kantarek <jeff.kantarek@gmail.com>
2017-04-10 21:44:46 -0500

    [SOCKET] add JsonSvc.Sockets.Handler

--- lib/sockets/handler.ex
+++ lib/sockets/handler.ex
@@ -1,49 +1,80 @@
-defmodule AccountRequest.Socket do
-  use Phoenix.Socket
+defmodule JsonSvc.Sockets.Endpoint do
 
-  ## Channels
-  channel "request:*", AccountRequest.RequestChannel
+  use Phoenix.Endpoint, otp_app: :json_svc_ex_sockets
 
-  ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+    socket :_, JsonSvc.Sockets
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
-  end
+    def init(_, _req, _opts) do
+      {:upgrade, :protocol, :cowboy_websocket}
+    end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "users_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     JsonWebsockets.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+    @timeout 60_000 # terminate if no activity for one minute
+
+    #Called on websocket connection initialization.
+    def websocket_init(_type, req, _opts) do
+      state = %{}
+      {:ok, req, state, @timeout}
+    end
+
+    # Handle 'ping' messages from the browser - reply
+    def websocket_handle({:text, "ping"}, req, state) do
+      {:reply, {:text, "pong"}, req, state}
+    end
+
+    def websocket_handle({:text, message}, req, %{channel: "room:" <> name}) do
+      {:noreply, socket} = JsonSvc.Channels.Room.handle_in("new_msg", %{"body" => message}, req, %{channel: "room:"<>name})
+      {:ok, socket, %{channel: "room:"<>name}}
+    end
+
+    def websocket_handle({:text, "room:" <> name}, req, state) do
+      {:ok, socket} = JsonSvc.Channels.Room.join("room:" <> name, get_socket_keyp(req), req)
+      {:ok, socket, %{channel: "room:" <> name}}
+    end
+
+    # Handle other messages from the browser - don't reply
+    def websocket_handle({:text, message}, req, state) do
+      {:ok, req, state}
+    end
+
+    # Format and forward elixir messages to client
+    def websocket_info(message, req, state) do
+      {:reply, {:text, message}, req, state}
+    end
+
+    # No matter why we terminate, remove all of this pids subscriptions
+    def websocket_terminate(_reason, _req, _state) do
+      :ok
+    end
+
+    def get_socket_keyp(socket) do
+      {:http_req, port_id, :ranch_tcp, :keepalive,
+       socket_pid, _method, :"HTTP/1.1",
+       {socket_ip, socket_port}, _server_host, _undefined, _4000,
+       "/ws", _ndefined, _, _undefined, _array,
+       [{"connection", "Upgrade"}, {"upgrade", "websocket"},
+        {"host", "localhost:4000"},
+        {"sec-websocket-version", "13"},
+        {"sec-websocket-key", socket_key},
+        {"sec-websocket-extensions",
+         "permessage-deflate; client_max_window_bits"}],
+       _, _, _, _, _, _, _, _, _, _,_
+       } = socket
+      socket_key
+    end
 end
 
-defmodule AccountRequest.RequestChannel do
-  use Phoenix.Channel
+defmodule JsonSvc.Sockets do
+  use Phoenix.Socket
 
-  def join("request:lobby", _message, socket) do
-    {:ok, socket}
+  transport :websocket, Phoenix.Transports.WebSocket
+
+  ## Channels
+  channel "room:*", JsonSvc.Channels.Room
+
+  def connect(params, socket) do
+    {:ok, assign(socket, :user_id, params["user_id"])}
   end
 
-  def join("request:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
-  end
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
+
 end
